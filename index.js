fb5d3e35b271d920ce2ac45a647ac751113324bc
ejhem <113192777+ejhem@users.noreply.github.com>
2023-01-05 11:35:45 -0400

    added the markup template and tags and git ignore, installed inquirer

--- index.js
+++ index.js
@@ -1,99 +1,102 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateReadme = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>${name}'s Portfolio</title>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
-      />
-    </head>
-    <body>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <h1 class="title">${name} ${lastName}</h1>
-        </div>
-  
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a href="https://www.linkedin.com/in/${linkedin}" class="navbar-item">
-              LinkedIn
-            </a>
-  
-            <a href="https://github.com/${github}" class="navbar-item">
-              Github
-            </a>
-          </div>
-        </div>
-      </nav>
-  
-      <section class="section">
-        <div class="container">
-          <p class="subtitle">Welcome to my Portfolio!</p>
-        </div>
-        <p>Hello my name is ${name}, I currently live in ${location}.</p>
-        <p>I am currently learning to code in ${learningLang}.</p>
-        <p>I know already how to code in ${languages}.</p>
-      </section>
-  
-      <footer class="footer">
-        <div class="content has-text-centered">
-          <p>
-            To learn more about me, please checkout out my
-            <a href="https://github.com/${github}"> Github</a>
-            or my
-            <a href="https://www.linkedin.com/in/${linkedin}">
-              LinkedIn
-            </a>
-            or send me an email at: ${email}
-          </p>
-        </div>
-      </footer>
-    </body>
-  </html>`;
+const generateReadme = ({ title, name, description, installation, usage, license, email, tests, github, linkedin }) =>
+  `# ${title}
 
-  inquirer
+  ## Description
+
+  Hello my name is ${name}, and this is my project.
+  
+  ${description}
+  
+  ## Installation
+  
+  To install this application you will need to ${installation}
+  
+  ## Usage
+  
+  This is how you use this application:
+  ${usage}
+  
+  ## License
+  
+  I used ${license} for this project.
+
+  ## How to Contribute
+
+  If you have any suggestions on how this runs, please contact me at ${email}.
+
+  
+  ## Tests
+  
+  To test this yourself, ${tests}
+  
+  ## Questions and Contact
+
+  For any further questions and contact information, please email me at ${email} or view the following
+  
+  My linkedIn: https://www.linkedin.com/in/${linkedin}
+  My Github: https://github.com/${github}`;
+
+inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
     },
     {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      type: "input",
+      name: "name",
+      message: "What is your name?",
     },
     {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      type: "input",
+      name: "description",
+      message: "Please write a detailed description of what your project does."
     },
     {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      type: "input",
+      name: "installation",
+      message: "Please write a detailed explination of how to install this application."
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+      type: "input",
+      name: "usage",
+      message: "Please write a detailed description of how to use this application."
     },
     {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      type: "input",
+      name: "tests",
+      message: "Please write a detailed description on how to test this application."
+    },
+    {
+      type: "list",
+      message: "Please select the type of license you used for this application.",
+      name: "license",
+      choices: ['Apache 2.0', 'GNU 3.0', 'MIT', 'BSD 2-Clause', 'BSD 3-Clause', 'Boost 1.0', 'Creative Commons Zero 1.0', 'Eclipse 2.0'],
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username.",
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL.",
     },
   ])
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const readmeContent = generateReadme(answers);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    fs.writeFile("newREADME.md", readmeContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created newREADME.md!")
     );
   });
